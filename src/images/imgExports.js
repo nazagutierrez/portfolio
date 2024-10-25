e5db69a9622ba9eb68de109123f6cdbdbd525e06
Nazareno Gutierrez <nazarenojunin@gmail.com>
2024-10-25 17:04:17 -0300

    Optimized assets

--- src/images/imgExports.js
+++ src/images/imgExports.js
@@ -8,19 +8,13 @@ import gitImg from "./icons8-git-96.png";
 import apiImg from "./icons8-api-64.png";
 import framerMotion from "./framer-motion-icon.png";
 import responsiveDesign from "./icons8-responsive-64.png";
-import taskApp from "./tasks-app-image.png";
-import coinbase from "./coinbase-app-image.png";
-import portfolio from "./portfolio-img.png";
-import nextjs from "./next-js.png";
+import nextjs from "./next-js.webp";
 import firebase from "./firebase.svg";
 import tailwindcss from "./tailwind-css.svg";
-import cookta from "./cookta-foto.png";
-import profile from "./fotonaza3.jpg";
-import noBgProfile from "./fotonaza3-no-bg.png";
-import mensHousePicture from "./mens-house-picture.png";
-import mensHouseLogo from "./MARCADEAGUANEGRA1.png";
+import noBgProfile from "./fotonaza3-no-bg.webp";
+import mensHouseLogo from "./MARCADEAGUANEGRA1.webp";
 import tpeo from "./tpeo.png";
-import noCountry from "./no-country.png";
+import noCountry from "./no-country.webp";
 
 export default {
   reactImg,
@@ -33,16 +27,10 @@ export default {
   apiImg,
   framerMotion,
   responsiveDesign,
-  taskApp,
-  coinbase,
-  portfolio,
   nextjs,
   firebase,
   tailwindcss,
-  cookta,
-  profile,
   noBgProfile,
-  mensHousePicture,
   tpeo,
   noCountry,
   mensHouseLogo
