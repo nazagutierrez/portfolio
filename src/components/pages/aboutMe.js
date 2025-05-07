a775c71d79c22239f6ad3ab8880a4b732b5538ea
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-05-07 19:03:19 -0300

    Added lite-youtube to exp section

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -35,37 +35,6 @@ function AboutMe() {
             {t("about.description3")}
           </p>
         </div>
-        {/* <div className=" d-md-inline-block align-items-center mt-5 d-md-inline-block text-center mb-3 mb-md-0 w-100">
-            <h4 className="text-white fw-light fst-italic ms-lg-2">
-              {t("about.experience-title")}
-            </h4>
-            <div className="d-flex justify-content-center align-items-center gap-3 flex-xl-nowrap flex-wrap">
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item bg-transparent text-white border-warning">
-                  {t("about.experience.exp1")} 
-                </li>
-                <li className="list-group-item bg-transparent text-white border-warning ">
-                  {t("about.experience-description.exp1")}
-                </li>
-              </ul>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item bg-transparent text-white border-warning">
-                  {t("about.experience.exp2")} 
-                </li>
-                <li className="list-group-item bg-transparent text-white border-warning ">
-                  {t("about.experience-description.exp2")}
-                </li>
-              </ul>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item bg-transparent text-white border-warning">
-                  {t("about.experience.exp3")} 
-                </li>
-                <li className="list-group-item bg-transparent text-white border-warning ">
-                  {t("about.experience-description.exp3")}
-                </li>
-              </ul>
-            </div>
-        </div> */}
       </div>
     </div>
   );
--- src/components/pages/projects/cards.js
+++ src/components/pages/projects/cards.js
@@ -3,11 +3,13 @@ import { useTranslation } from "react-i18next";
 import imageImp from "../../../images/imgExports";
 import Timeline from "./Timeline";
 import ExpTech from "./ExpTech";
+import '@justinribeiro/lite-youtube';
 
 let cards = [
   {
     id: "tpeo",
     title: "TPEOficial",
+    videoUrl: "qkA1QbCKzRg",
     period: "2024 - Present",
     url: "https://github.com/TPEOficial",
     image: imageImp.tpeo,
@@ -15,6 +17,7 @@ let cards = [
   {
     id: "no-country",
     title: "No country",
+    videoUrl: "DjSyUzYUuTM",
     period: "2023 - 2024",
     url: "https://github.com/No-Country-simulation",
     image: imageImp.noCountry,
@@ -22,6 +25,7 @@ let cards = [
   {
     id: "mens-house-barber",
     title: "Men's House barber",
+    videoUrl: "1_Gtg7te6B8",
     period: "2022 - 2023",
     url: "https://github.com/nazagutierrez/mens-house-barber",
     image: imageImp.mensHouseLogo,
@@ -36,27 +40,34 @@ function Cards() {
         <Timeline />
         <div style={{gap: "50px"}} className="d-flex flex-column">
           {cards.map((card, index) => (
-            <div
-            style={{ maxWidth: "800px" }}
-            key={index}
-            className="d-flex flex-column align-items-start justify-content-center"
-            >
-              <h2 style={{ paddingBlock: "2px" }} className="bg-dark px-2 rounded-1 mb-2 exp-period">
-                {t(`experience.exp-period-${index}`)}
-              </h2> 
-              <a href={card.url} className="d-flex align-items-center mb-2 exp-link" target="_blank" rel="noreferrer">
-                <img className="exp-card-img me-2 d-sm-none rounded-1" src={card.image} alt="tpeo"/>
-                {card.title}
-                <svg style={{ fontSize: "1rem", color: "#2e2e2e9d", marginLeft: "8px" }} stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M14.78 3.653a3.936 3.936 0 1 1 5.567 5.567l-3.627 3.627a3.936 3.936 0 0 1-5.88-.353.75.75 0 0 0-1.18.928 5.436 5.436 0 0 0 8.12.486l3.628-3.628a5.436 5.436 0 1 0-7.688-7.688l-3 3a.75.75 0 0 0 1.06 1.061l3-3Z"></path><path d="M7.28 11.153a3.936 3.936 0 0 1 5.88.353.75.75 0 0 0 1.18-.928 5.436 5.436 0 0 0-8.12-.486L2.592 13.72a5.436 5.436 0 1 0 7.688 7.688l3-3a.75.75 0 1 0-1.06-1.06l-3 3a3.936 3.936 0 0 1-5.567-5.568l3.627-3.627Z"></path></svg>
-              </a>
-              <div className="d-flex align-items-center">
-                <img className="exp-card-img d-none d-sm-block" src={card.image} alt="tpeo"/>
-                <div className="d-flex flex-column align-items-center ps-4">
-                  <p style={{ lineHeight: "29px", marginBottom: "15px"}} className="fs-6 exp-description">
-                    {t(`experience.exp-description-${index}`)}
-                  </p>
-                  <ExpTech expName={card.id} />
+            <div className="d-flex align-items-start justify-content-center" key={index}>
+              <div
+              style={{ maxWidth: "800px" }}
+              key={index}
+              className="d-flex flex-column align-items-start justify-content-center"
+              >
+                <h2 style={{ paddingBlock: "2px" }} className="bg-dark px-2 rounded-1 mb-2 exp-period">
+                  {t(`experience.exp-period-${index}`)}
+                </h2> 
+                <a href={card.url} className="d-flex align-items-center mb-2 exp-link" target="_blank" rel="noreferrer">
+                  <img className="exp-card-img me-2 d-sm-none rounded-1" src={card.image} alt="tpeo"/>
+                  {card.title}
+                  <svg style={{ fontSize: "1rem", color: "#2e2e2e9d", marginLeft: "8px" }} stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M14.78 3.653a3.936 3.936 0 1 1 5.567 5.567l-3.627 3.627a3.936 3.936 0 0 1-5.88-.353.75.75 0 0 0-1.18.928 5.436 5.436 0 0 0 8.12.486l3.628-3.628a5.436 5.436 0 1 0-7.688-7.688l-3 3a.75.75 0 0 0 1.06 1.061l3-3Z"></path><path d="M7.28 11.153a3.936 3.936 0 0 1 5.88.353.75.75 0 0 0 1.18-.928 5.436 5.436 0 0 0-8.12-.486L2.592 13.72a5.436 5.436 0 1 0 7.688 7.688l3-3a.75.75 0 1 0-1.06-1.06l-3 3a3.936 3.936 0 0 1-5.567-5.568l3.627-3.627Z"></path></svg>
+                </a>
+                <div className="d-flex align-items-center">
+                  <img className="exp-card-img d-none d-sm-block" src={card.image} alt="tpeo"/>
+                  <div className="d-flex flex-column align-items-center ps-4">
+                    <p style={{ lineHeight: "29px", marginBottom: "15px"}} className="fs-6 exp-description">
+                      {t(`experience.exp-description-${index}`)}
+                    </p>
+                    <ExpTech expName={card.id} />
+                  </div>
                 </div>
+              </div>
+              <div className="">
+                <lite-youtube videoid={card.videoUrl} >
+                  <a class="lite-youtube-fallback" href={`https://youtu.be/${card.videoUrl}`}>Watch on YouTube: "Sample output of devtools-to-video cli tool"</a>
+                </lite-youtube>
               </div>
             </div>
           ))}
