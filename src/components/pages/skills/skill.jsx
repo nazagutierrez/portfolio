dd537c3fe06a3e0fa04507ded884d14bd5f03bb8
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-05-09 17:54:21 -0300

    Improved code

--- src/components/pages/skills/skill.jsx
+++ src/components/pages/skills/skill.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 function Skill({ image, title }) {
   return (
-    <div className="text-white text-center flex flex-col justify-center items-center">
-      <div className="skill-img-container bg-[#1f1f1f] py-2 px-6 rounded-sm">
+    <div className="text-white-main text-center w-full flex flex-col justify-center items-center">
+      <div className="skill-hover-effect bg-bg-black py-2 px-6 rounded-sm">
         <img
           src={image}
-          className="map-img my-2"
+          className="my-2 w-[100px] h-16 object-contain"
           alt={title}
         />
       </div>
-      <h3 className="text-black mt-1 skill-title">{title}</h3>
+      <h3 className="text-black-main mt-1 font-medium max-w-[140px]">{title}</h3>
     </div>
 
   );
