5357e2212d1a4b68980a1a2a7b8d0597b95cbac5
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-05-10 19:08:50 -0300

    Responsive changes

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -51,13 +51,13 @@ export function Home() {
       <div ref={mainRef}>
         <h1
           ref={titleRef}
-          className="flex justify-center text-center mb-2 text-white-main mt-32 text-5xl"
+          className="flex justify-center px-2 text-center mb-2 text-white-main mt-5 lg:mt-32 text-4xl sm:text-5xl"
         >
           Nazareno Gutierrez
         </h1>
         <p
           ref={subtitleRef}
-          className="flex justify-center mb-14 text-center text-gray-main italic text-xl font-light"
+          className="flex justify-center mb-4 sm:mb-14 text-center text-gray-main italic text-base sm:text-xl font-light"
         >
           {t("home.frontend")}
         </p>
@@ -83,7 +83,7 @@ export function Home() {
 
         <div className="flex gap-2 flex-wrap justify-center mt-2">
           <a
-            className="main-btn pointer px-3 py-1 font-medium rounded-tr-md rounded-bl-md"
+            className="main-btn pointer text-sm sm:text-base px-1 sm:px-3 py-1 font-medium rounded-tr-md rounded-bl-md"
             href={resumeEN}
             target="_blank"
             rel="noreferrer"
@@ -91,7 +91,7 @@ export function Home() {
             Resume EN
           </a>
           <a
-            className="main-btn px-3 py-1 font-medium rounded-tr-md rounded-bl-md"
+            className="main-btn text-sm sm:text-base px-1 sm:px-3 py-1 font-medium rounded-tr-md rounded-bl-md"
             href={resumeES}
             target="_blank"
             rel="noreferrer"
