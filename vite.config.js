0f75c6522db0b7492e4df2202fec3a61a66c35cb
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-05-14 20:55:15 -0300

    Fixed deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // 👈 esta línea es clave para GitHub Pages
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
   },
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   }
 })
