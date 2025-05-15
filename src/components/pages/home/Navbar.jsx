d1bf340cf295838d9b061ac2a3f40b6135f89970
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-05-15 16:43:08 -0300

    Removed unused code

--- src/components/pages/home/Navbar.jsx
+++ src/components/pages/home/Navbar.jsx
@@ -27,27 +27,27 @@ export const Navbar = () => {
       >
         <div className="flex justify-center w-full h-full">
           <ul className="flex navbar-nav items-center space-x-6 text-white-main text-sm">
-            <li className="nav-item">
+            <li>
               <ScrollLink to="#Home" className="nav-link">
                 {t("home.nav-link-1")}
               </ScrollLink>
             </li>
-            <li className="nav-item">
+            <li>
               <ScrollLink to="#Experience" className="nav-link">
                 {t("home.nav-link-2")}
               </ScrollLink>
             </li>
-            <li className="nav-item">
+            <li>
               <ScrollLink center={true} to="#About" className="nav-link">
                 {t("home.nav-link-3")}
               </ScrollLink>
             </li>
-            <li className="nav-item">
+            <li>
               <ScrollLink center={true} to="#Skills" className="nav-link">
                 {t("home.nav-link-4")}
               </ScrollLink>
             </li>
-            <li className="nav-item">
+            <li>
               <ScrollLink to="#Contact" className="nav-link">
                 {t("home.nav-link-5")}
               </ScrollLink>
